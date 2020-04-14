f699102193398cb3efd0c2138403ca651c03a07b
Reilley Luedde <51122690+rluedde@users.noreply.github.com>
2020-04-13 21:08:12 -0700

    Remove option to magicalize 0 times

--- magical_function.py
+++ magical_function.py
@@ -5,11 +5,10 @@ import time
 
 def magicalize():
     magic_me = input("Hey user, what word do you want to see MAGICALIZED? ")
-    times_to_magic = random.randint(0, 10)
+    times_to_magic = random.randint(1, 10)
     newlines = random.randint(1,3) * "\n"
     for i in range(0, times_to_magic):
         before_spaces = random.randint(0,25) * " "
         after_spaces = random.randint(0,25) * " "
         print(before_spaces + "*" + magic_me + "*" + after_spaces + newlines)
-
 
